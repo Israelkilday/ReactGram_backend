989878fc781b9cd95c1f1db42b79c905fbe9cc81
Israel Kilday <israel.kilday@yahoo.com.br>
2024-02-20 21:13:07 -0300

    feat: creat files db.js,User.js,router.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+ 
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -11,6 +11,20 @@ const app = express();
 // config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+// solve cors
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+// upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// db connection
+require("./config/db.js");
+
+// routes
+const router = require("./routes/router.js");
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);
---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.28audbl.mongodb.net/?retryWrites=true&w=majority`
+        );
+
+        console.log("Conectou com o banco!")
+
+        return dbConn;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+conn();
+
+
+
+// conection
+// L0LASFLCrZtAbTjM password mongodb
+// kLEQ0UObatFDXY2q password reactgram
